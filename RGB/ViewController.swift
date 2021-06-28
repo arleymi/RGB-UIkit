ba44936cd856330bece5f6e0fad0c9393a95c31e
Arthur <arthur.lee290499@gmail.com>
2021-06-29 03:26:28 +0600

    StoryBoard, var with outlets

--- RGB/ViewController.swift
+++ RGB/ViewController.swift
@@ -8,11 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var colorView: UIView!
+    
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redTextField: UITextField!
+    @IBOutlet var greenTextField: UITextField!
+    @IBOutlet var blueTextField: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        colorView.layer.cornerRadius = 17
+        
+        redSlider.tintColor = .red
+        greenSlider.tintColor = .green
+        blueSlider.tintColor = .blue
+        
+        
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func rgbSliders(_ sender: UISlider) {
+        
+    }
+    
 
 
 }
